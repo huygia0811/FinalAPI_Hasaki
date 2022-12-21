a39973aa46ab70b201f0b8fe69dec1cd2bf9b589
huygia0811 <20520203@gm.uit.edu.vn>
2022-12-21 20:38:06 +0700

    Connect database and add proc getallsanpham

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FinalAPI_Hasaki.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        [Route("api/ServiceController/GetAllSanPham")]
+        [HttpGet]
+        public IHttpActionResult GetAllSanPham()
+        {
+            try
+            {
+                DataTable result = Database.Database.ReadTable("Proc_GetAllSanPham");
+                return Ok(result);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
